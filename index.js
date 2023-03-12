eb26b488a25fcb5c2f4b88bd1992ee0efe92fd5e
Aizen <iansky3311@gmail.com>
2023-03-12 15:27:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards (names, gift) {
+    const ary =[]
+    for (let i = 0; i < names.length; i++) {
+      ary.push( `Thank you, ${names[i]}, for the wonderful ${gift} gift!`);
+}
+return ary;
+}
+function countDown(c){
+  while (c >= 0){
+    console.log(c--)
+  }
+}
